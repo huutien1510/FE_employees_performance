c8eb27904c396642d08d6def540f20ec5d02e486
huutien1510 <huutien1510@gmail.com>
2025-04-08 10:56:58 +0700

    login 8/4 11h

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PerformanceEvaluation from './PerformanceEvaluation.jsx'
+import LoginPage from './LoginPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LoginPage />
   </StrictMode>,
 )
